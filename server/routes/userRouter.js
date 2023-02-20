663b2b907a94e19009676408cf8263454a78ba28
maixuany <maixuanytp3@gmail.com>
2023-02-20 19:55:21 +0700

    add router for user

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,5 +4,7 @@ const auth = require("../middlewares/auth");
 const userRouter = express.Router();
 
 userRouter.get("/me", auth, userController.getme);
+userRouter.patch("/change_pass", auth, userController.changePassword);
+userRouter.patch("/change_info", auth, userController.edit);
 
 module.exports = userRouter;
