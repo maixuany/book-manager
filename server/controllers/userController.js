96256e1a02c02ead5b595a5f02dd968ff4055aaa
maixuany <maixuanytp3@gmail.com>
2023-02-20 10:21:38 +0700

    update userCtroller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,49 @@
+const httpStatus = require("http-status");
+const bcrypt = require("bcrypt");
 const userSch = require("../models/UserSch");
 const userController = {};
+
+userController.edit = async (req, res) => {
+  try {
+    const { fullname, username } = req.data;
+    const me = await userSch.findOne({ username: username });
+    const new_fullname = req.body.fullname;
+    me.fullname = new_fullname;
+    await me.save();
+    const { _id, password, access_token, ...dataRes } = me._doc;
+    return res
+      .status(httpStatus.OK)
+      .send({ data: dataRes, message: "changed success".toUpperCase() });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+      message: "INTERNAL_SERVER_ERROR ".toUpperCase() + error.toString(),
+    });
+  }
+};
+
+userController.changePassword = async (req, res) => {
+  try {
+    const { fullname, username } = req.data;
+    const me = await userSch.findOne({ username: username });
+    const { oldPassword, newPassword } = req.body;
+    if (!bcrypt.compareSync(oldPassword, me.password)) {
+      return res
+        .status(httpStatus.FORBIDDEN)
+        .send({ message: "Wrong password".toUpperCase() });
+    } else {
+      const salt = bcrypt.genSaltSync();
+      const hashPassword = bcrypt.hashSync(newPassword, salt);
+      me.password = hashPassword;
+      me.save();
+      return res
+        .status(httpStatus.OK)
+        .send({ data: dataRes, message: "changed success".toUpperCase() });
+    }
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+      message: "INTERNAL_SERVER_ERROR ".toUpperCase() + error.toString(),
+    });
+  }
+};
 
 module.exports = userController;
