2c7677d6f712053357c03718a1cea1f937dbccfc
maixuany <maixuanytp3@gmail.com>
2023-02-22 16:23:41 +0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Admin, Resource } from "react-admin";
+import BookList from "./components/book/books";
+import jsonServerProvider from "ra-data-json-server";
 
 function App() {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/api/v1/book")
+      .then((response) => response.json())
+      .then((payload) => setBooks(payload.data))
+      .catch((error) => console.log(error.message));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Admin>
+      <Resource name="books" list={BookList} data={books} />
+    </Admin>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const bodyParser = require("body-parser");
+const requestIp = require("request-ip");
 require("dotenv").config();
 
 const router = require("./routes/index");
@@ -19,6 +20,18 @@ app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1", router);
+
+app.use(function (req, res, next) {
+  req.client_ip_address = requestIp.getClientIp(req);
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "DELETE, GET, POST, PUT, PATCH");
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
